aa06bfcc6ce8f6cf607e922b0d15d2dc88f75cc1
cpojer <christoph.pojer@gmail.com>
2012-05-02 01:36:32 +0200

    Adding base functionality for Login screen.

--- App/JavaScript/App.js
+++ App/JavaScript/App.js
@@ -2,27 +2,35 @@
 
 var router = new Router();
 
-router.add('login', Controller.lookup('login'));
+router.add('/login', Controller.lookup('login'));
 
+var baseURL = '/Thesis/App/';
+var baseRegex = /^\/Thesis\/App\//;
 History.addEvent('change', function(url) {
-  console.log('hey', url);
+  if (baseRegex.test(url)) url = url.substr(baseURL.length);
+  if (!(/^\//).test(url)) url = '/' + url;
+
+  console.log('going to', url);
   router.parse(url);
 });
 
 var boot = function() {
-  
+
   window.scrollTo(0, 1);
   (new ActiveState()).attach();
-  
+
   var isLoggedIn = false;
-  if (!isLoggedIn) History.push('login');
+  // Browser bug: prevent this from firing twice in Chrome
+  if (!isLoggedIn) setTimeout(function() {
+    History.push('login');
+  }, 100);
 
   var counter = 0;
   var isRecording = false;
   var button = document.id('record');
   var play = document.id('play');
   var timer, media, fs;
-    
+
   var success = function() {
     play.style.display = 'block';
     console.log('success');
@@ -35,12 +43,12 @@ var boot = function() {
   var updateTime = function() {
     document.id('counter').innerText = (++counter) + ' seconds';
   };
-  
+
   var successFolderCreation = function(folder) {
     var file = folder.getFile('recording.mp3', {create: true, exclusive: true});
 
     media = new Media(file, success, error);
-  
+
     if (isRecording) {
       record.innerText = 'record';
       media.stopRecord();
@@ -54,10 +62,10 @@ var boot = function() {
     media.startRecord();
     isRecording = true;
     record.innerText = 'stop';
-      
+
     timer = setInterval(updateTime, 1000);
   };
-  
+
   var fn = function(fileSystem) {
     fs = fileSystem;
     fs.root.getDirectory('media', {create: true}, successFolderCreation);
@@ -67,10 +75,10 @@ var boot = function() {
     event.preventDefault();
     media.play();
   }, false);
-  
+
   button.addEventListener('click', function(event) {
     event.preventDefault();
-    
+
     if (fs) fn(fs);
     else window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fn, null);
   }, false);
--- App/JavaScript/Controllers/Login.js
+++ App/JavaScript/Controllers/Login.js
@@ -1,6 +1,37 @@
-Controller.define('login', function() {
+(function() {
 
-  UI.lock();
-  document.id('login').show();
+API.on('login/submit').addEvents({
+
+  success: function() {
+    document.id('main').show();
+
+    var login = document.id('login');
+    var splash = document.id('splash');
+
+    login.addClass('fade').addEvent('transformComplete', function() {
+      login.hide();
+      splash.hide();
+    });
+    splash.addClass('fade');
+
+    UI.unlock();
+  },
+
+  error: function() {
+
+  }
 
 });
+
+Controller.define('login', function() {
+
+  var login = document.id('login');
+
+  new Form(document.id('loginAction'), 'login/submit');
+
+  UI.lock();
+  login.show();
+
+});
+
+})();
