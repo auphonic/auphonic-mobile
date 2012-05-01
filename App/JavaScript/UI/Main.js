88c91610c10c203501b54079b5fae03d1193bd4f
cpojer <christoph.pojer@gmail.com>
2012-05-02 01:32:01 +0200

    UI.lock should prevent scrolling.

--- App/JavaScript/UI/Main.js
+++ App/JavaScript/UI/Main.js
@@ -1,15 +1,21 @@
 (function() {
 
+var preventDefault = function(event) {
+  event.preventDefault();
+};
+
 this.UI = {
-  
+
   lock: function() {
     document.body.addClass('lock');
+    window.addEvent('touchmove', preventDefault);
   },
-  
+
   unlock: function() {
     document.body.removeClass('lock');
+    window.removeEvent('touchmove', preventDefault);
   }
-  
+
 };
 
 })();
