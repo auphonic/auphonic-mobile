b5c349fd21b60463203112f30abe16a860eed114
cpojer <christoph.pojer@gmail.com>
2012-05-14 04:06:32 +0200

    Updating PreventClickOnScroll. It is quite experimental and hacky but it works in more use-cases now. Still not 100 % awesome :(

--- App/JavaScript/Library/Browser/PreventClickOnScroll.js
+++ App/JavaScript/Library/Browser/PreventClickOnScroll.js
@@ -1,13 +1,16 @@
 (function() {
 
-var events = ['touchstart', 'touchmove', 'touchend', 'touchcancel'];
+var events = ['touchstart', 'touchmove', 'touchcancel', 'touchend'];
 
 this.PreventClickOnScroll = new Class({
 
   Implements: Class.Binds,
 
   start: 0,
+  scrolling: false,
   enabled: false,
+  cancelOnTouchEnd: false,
+  attachOnTouchEnd: false,
 
   initialize: function(selector) {
     this.selector = selector;
@@ -28,22 +31,33 @@ this.PreventClickOnScroll = new Class({
   touchstart: function(event) {
     if (event.touches.length > 1) return;
 
-    var node = event.target;
-    if (node.nodeType == 3) node = node.parentNode;
-    this.enabled = !!(node.webkitMatchesSelector(this.selector) || node.getParent(this.selector));
+    if (this.scrolling) {
+      clearTimeout(this.timer);
+      this.cancelOnTouchEnd = true;
+      if (this.node) this.node.removeEventListener('scroll', this.bound('listener'), false);
+    }
 
+    this.enabled = !!this.getNode(event.target);
     this.start = event.touches[0].pageY;
   },
 
   touchmove: function(event) {
     var end = event.touches[0].pageY;
-    // Allows a small delta to detect movement
-    if (this.enabled && end && Math.abs(this.start - end) > 3)
-      Element.disableCustomEvents();
+    if (!this.enabled || !end || Math.abs(this.start - end) <= 3) return;
+
+    Element.disableCustomEvents.delay(0);
+    this.attachOnTouchEnd = true;
   },
 
-  touchend: function() {
-    this.cancel();
+  touchend: function(event) {
+    if (this.attachOnTouchEnd) {
+      var node = this.getNode(event.target);
+      node.addEventListener('scroll', this.bound('listener'), false);
+      this.node = node;
+    }
+
+    if (this.cancelOnTouchEnd)
+      this.cancel();
   },
 
   touchcancel: function() {
@@ -51,10 +65,22 @@ this.PreventClickOnScroll = new Class({
   },
 
   cancel: function() {
-    this.enabled = true;
-    (function() {
-      Element.enableCustomEvents();
-    }).delay(1);
+    this.scrolling = false;
+    this.cancelOnTouchEnd = false;
+    Element.enableCustomEvents.delay(0);
+    if (this.node) this.node.removeEventListener('scroll', this.bound('listener'), false);
+    this.node = null;
+  },
+
+  listener: function() {
+    this.scrolling = true;
+    clearTimeout(this.timer);
+    this.timer = this.cancel.delay(300, this);
+  },
+
+  getNode: function(node) {
+    if (node.nodeType == 3) node = node.parentNode;
+    return node.match(this.selector) ? node : node.getParent(this.selector);
   }
 
 });
