1510c81866241c506cf874f2b9ca74be46f186a6
cpojer <christoph.pojer@gmail.com>
2012-05-14 05:21:58 +0200

    More monkey-patching for Dev.js

--- App/JavaScript/Dev.js
+++ App/JavaScript/Dev.js
@@ -1,16 +1,24 @@
-(function(push, onChange) {
+(function(push, onChange, getPath) {
 
 // Monkey Patching for dev.
 if (this.__DEV__) {
   var baseURL = this.__baseURL;
   var base = this.__base;
+  var clean = function(url) {
+    return base.test(url) ? url.substr(baseURL.length) : url;
+  };
+
   History.push = function(url, title, state) {
     push.call(this, baseURL + url.replace(/^\//, ''), title, state);
   };
 
   History.onChange = function(url, state) {
-    onChange.call(this, base.test(url) ? url.substr(baseURL.length) : url, state);
+    onChange.call(this, clean(url), state);
+  };
+
+  History.getPath = function() {
+    return clean(getPath.call(this));
   };
 }
 
-})(History.push, History.onChange);
+})(History.push, History.onChange, History.getPath);
