c1730fb9309fe1e6619cf32678a7975913627f61
cpojer <christoph.pojer@gmail.com>
2012-11-13 17:25:40 +0100

    Minor changes.

--- JavaScript/Auphonic.js
+++ JavaScript/Auphonic.js
@@ -1,4 +1,4 @@
-exports.Version = '0.91';
+exports.Version = '1.0.1';
 exports.FeedbackURL = 'mailto:christoph.pojer+auphonic@gmail.com?subject=Auphonic%20Mobile%20App%20Feedback';
 exports.RepositoryURL = 'https://github.com/auphonic/auphonic-mobile';
 exports.IssuesURL = 'https://github.com/auphonic/auphonic-mobile/issues';
