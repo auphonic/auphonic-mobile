e8520ca0354e2423b2b4bdb7d982a8026f3b35e8
cpojer <christoph.pojer@gmail.com>
2013-02-14 14:48:15 +0100

    Refactor WebIntent.Java.

--- Android/src/com/borismus/WebIntent.java
+++ Android/src/com/borismus/WebIntent.java
@@ -15,131 +15,71 @@ import android.text.Html;
 
 import org.apache.cordova.api.Plugin;
 import org.apache.cordova.api.PluginResult;
+import org.apache.cordova.api.PluginResult.Status;
 
 /**
- * WebIntent is a PhoneGap plugin that bridges Android intents and web
- * applications:
- *
- * 1. web apps can spawn intents that call native Android applications. 2.
- * (after setting up correct intent filters for PhoneGap applications), Android
- * intents can be handled by PhoneGap web applications.
- *
- * @author boris@borismus.com
- *
+ * @originalAuthor boris@borismus.com
+ * @modifiedBy christoph.pojer@gmail.com
  */
 public class WebIntent extends Plugin {
 
   private String onNewIntentCallback = null;
 
-  /**
-   * Executes the request and returns PluginResult.
-   *
-   * @param action
-   *            The action to execute.
-   * @param args
-   *            JSONArray of arguments for the plugin.
-   * @param callbackId
-   *            The callback id used when calling back into JavaScript.
-   * @return A PluginResult object with a status and message.
-   */
+  private Map<String, String>getExtras(JSONObject extras) {
+    Map<String, String> map = new HashMap<String, String>();
+    if (extras == null) return map;
+    try {
+      JSONArray names = extras.names();
+      for (int i = 0; i < names.length(); i++) {
+        String key = names.getString(i);
+        map.put(key, extras.getString(key));
+      }
+    } catch(JSONException e) {}
+    return map;
+  }
+
   public PluginResult execute(String action, JSONArray args, String callbackId) {
     try {
-      if (action.equals("startActivity")) {
-        if (args.length() != 1) {
-          return new PluginResult(PluginResult.Status.INVALID_ACTION);
-        }
-
-        // Parse the arguments
+      if (action.equals("startActivity") && args.length() == 1) {
         JSONObject obj = args.getJSONObject(0);
-        String type = obj.has("type") ? obj.getString("type") : null;
-        Uri uri = obj.has("url") ? Uri.parse(obj.getString("url")) : null;
-        JSONObject extras = obj.has("extras") ? obj.getJSONObject("extras") : null;
-        Map<String, String> extrasMap = new HashMap<String, String>();
-
-        // Populate the extras if any exist
-        if (extras != null) {
-          JSONArray extraNames = extras.names();
-          for (int i = 0; i < extraNames.length(); i++) {
-            String key = extraNames.getString(i);
-            String value = extras.getString(key);
-            extrasMap.put(key, value);
-          }
-        }
-
-        startActivity(obj.getString("action"), uri, type, extrasMap);
-        return new PluginResult(PluginResult.Status.OK);
-
-      } else if (action.equals("hasExtra")) {
-        if (args.length() != 1) {
-          return new PluginResult(PluginResult.Status.INVALID_ACTION);
-        }
+        startActivity(
+          obj.getString("action"),
+          obj.has("url") ? Uri.parse(obj.getString("url")) : null,
+          obj.has("type") ? obj.getString("type") : null,
+          getExtras(obj.has("extras") ? obj.getJSONObject("extras") : null)
+        );
+        return new PluginResult(Status.OK);
+      } else if (action.equals("hasExtra") && args.length() == 1) {
+        Intent i = this.cordova.getActivity().getIntent();
+        return new PluginResult(Status.OK, i.hasExtra(args.getString(0)));
+      } else if (action.equals("getExtra") && args.length() == 1) {
         Intent i = this.cordova.getActivity().getIntent();
         String extraName = args.getString(0);
-        return new PluginResult(PluginResult.Status.OK, i.hasExtra(extraName));
-
-      } else if (action.equals("getExtra")) {
-        if (args.length() != 1) {
-          return new PluginResult(PluginResult.Status.INVALID_ACTION);
-        }
+        if (i.hasExtra(extraName)) return new PluginResult(Status.OK, i.getStringExtra(extraName));
+        else return new PluginResult(Status.ERROR);
+      } else if (action.equals("getURI") && args.length() == 0) {
         Intent i = this.cordova.getActivity().getIntent();
-        String extraName = args.getString(0);
-        if (i.hasExtra(extraName)) {
-          return new PluginResult(PluginResult.Status.OK, i.getStringExtra(extraName));
-        } else {
-          return new PluginResult(PluginResult.Status.ERROR);
-        }
-      } else if (action.equals("getURI")) {
-        if (args.length() != 0) {
-          return new PluginResult(PluginResult.Status.INVALID_ACTION);
-        }
-
-        Intent i = this.cordova.getActivity().getIntent();
-        String uri = i.getDataString();
-        return new PluginResult(PluginResult.Status.OK, uri);
-      } else if (action.equals("onNewIntent")) {
-        if (args.length() != 0) {
-          return new PluginResult(PluginResult.Status.INVALID_ACTION);
-        }
-
+        return new PluginResult(Status.OK, i.getDataString());
+      } else if (action.equals("onNewIntent") && args.length() == 0) {
         this.onNewIntentCallback = callbackId;
-        PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+        PluginResult result = new PluginResult(Status.NO_RESULT);
         result.setKeepCallback(true);
         return result;
-      } else if (action.equals("sendBroadcast")) {
-        if (args.length() != 1) {
-          return new PluginResult(PluginResult.Status.INVALID_ACTION);
-        }
-
-        // Parse the arguments
+      } else if (action.equals("sendBroadcast") && args.length() == 1) {
         JSONObject obj = args.getJSONObject(0);
-
-        JSONObject extras = obj.has("extras") ? obj.getJSONObject("extras") : null;
-        Map<String, String> extrasMap = new HashMap<String, String>();
-
-        // Populate the extras if any exist
-        if (extras != null) {
-          JSONArray extraNames = extras.names();
-          for (int i = 0; i < extraNames.length(); i++) {
-            String key = extraNames.getString(i);
-            String value = extras.getString(key);
-            extrasMap.put(key, value);
-          }
-        }
-
-        sendBroadcast(obj.getString("action"), extrasMap);
-        return new PluginResult(PluginResult.Status.OK);
+        sendBroadcast(obj.getString("action"), getExtras(obj.has("extras") ? obj.getJSONObject("extras") : null));
+        return new PluginResult(Status.OK);
       }
-      return new PluginResult(PluginResult.Status.INVALID_ACTION);
+      return new PluginResult(Status.INVALID_ACTION);
     } catch (JSONException e) {
-      e.printStackTrace();
-      return new PluginResult(PluginResult.Status.JSON_EXCEPTION);
+      return new PluginResult(Status.JSON_EXCEPTION);
     }
   }
 
   @Override
   public void onNewIntent(Intent intent) {
     if (this.onNewIntentCallback != null) {
-      PluginResult result = new PluginResult(PluginResult.Status.OK, intent.getDataString());
+      PluginResult result = new PluginResult(Status.OK, intent.getDataString());
       result.setKeepCallback(true);
       this.success(result, this.onNewIntentCallback);
     }
@@ -148,24 +88,21 @@ public class WebIntent extends Plugin {
   void startActivity(String action, Uri uri, String type, Map<String, String> extras) {
     Intent i = (uri != null ? new Intent(action, uri) : new Intent(action));
 
-    if (type != null && uri != null) i.setDataAndType(uri, type); //Fix the crash problem with android 2.3.6
-    else if (type != null) i.setType(type);
+    if (type != null) {
+      if (uri != null) i.setDataAndType(uri, type); //Fix the crash problem with android 2.3.6
+      else i.setType(type);
+    }
 
     for (String key : extras.keySet()) {
       String value = extras.get(key);
-      // If type is text html, the extra text must sent as HTML
-      if (key.equals(Intent.EXTRA_TEXT) && type.equals("text/html")) {
-        i.putExtra(key, Html.fromHtml(value));
-      } else if (key.equals(Intent.EXTRA_STREAM)) {
-        // allowes sharing of images as attachments.
-        // value in this case should be a URI of a file
-        i.putExtra(key, Uri.parse(value));
-      } else if (key.equals(Intent.EXTRA_EMAIL)) {
-        // allows to add the email address of the receiver
-        i.putExtra(Intent.EXTRA_EMAIL, new String[] { value });
-      } else {
+      if (key.equals(Intent.EXTRA_TEXT) && type.equals("text/html"))
+        i.putExtra(key, Html.fromHtml(value)); // If type is text html, the extra text must sent as HTML
+      else if (key.equals(Intent.EXTRA_STREAM))
+        i.putExtra(key, Uri.parse(value)); // allowes sharing of images as attachments. Value should be the URI of a file
+      else if (key.equals(Intent.EXTRA_EMAIL))
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{value}); // allows to add the email address of the receiver
+      else
         i.putExtra(key, value);
-      }
     }
     this.cordova.getActivity().startActivity(i);
   }
@@ -173,10 +110,8 @@ public class WebIntent extends Plugin {
   void sendBroadcast(String action, Map<String, String> extras) {
     Intent intent = new Intent();
     intent.setAction(action);
-    for (String key : extras.keySet()) {
-      String value = extras.get(key);
-      intent.putExtra(key, value);
-    }
+    for (String key : extras.keySet())
+      intent.putExtra(key, extras.get(key));
 
     this.cordova.getActivity().sendBroadcast(intent);
   }
